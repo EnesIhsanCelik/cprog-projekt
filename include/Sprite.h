8f2018b9410adea56288a9c87ad5c106b017fcb2
Enes <enesihsancelik@gmail.com>
2025-12-15 16:09:10 +0100

    Making some adjustments in header file

--- include/Sprite.h
+++ include/Sprite.h
@@ -1,27 +1,30 @@
+#pragma once
 #include <SDL3/SDL.h>
+#include <memory>
 
 class Sprite
 {
 public:
-    SDL_FRect rect;
-    SDL_Texture* texture;   
+    using Ptr = std::shared_ptr<Sprite>;  
     
     Sprite(SDL_Texture* texture, float x, float y, float w, float h);
     virtual ~Sprite();
 
-    void draw(SDL_Renderer* renderer);
+    void draw(SDL_Renderer* renderer) const;
     virtual void update(float dt) = 0;
     
     void move(float dx, float dy) { rect.x += dx; rect.y += dy; }
-    SDL_FPoint velocity {0.0f, 0.0f}; // Automatisk rörelse för bullets och fiender
 
     bool intersects(const Sprite& other) const;
     virtual void onCollision(Sprite* other) {};
+    
     void destroy() { alive = false; }
     bool isAlive() const { return alive; }
 
     const SDL_FRect& getRect() const { return rect; }
 
-private:
+protected:
+    SDL_FRect rect;
+    SDL_Texture* texture; 
     bool alive = true;
 };
