4fcbd5ee3048298d2f0028dd006f09ede4776aff
jononor <john.nordstrom500@outlook.com>
2025-12-22 11:53:25 +0100

    namspac

--- include/Label.h
+++ include/Label.h
@@ -3,30 +3,24 @@
 #include <string>
 #include <memory>
 
-class Label;
-
-typedef std::shared_ptr<Label> LabelPtr;
-
-class Label: public Sprite {
-
-    public:
-    static LabelPtr make(float x, float y, float w, float h, std::string txt);
-    
-    ~Label() {};
-    
-    void draw() const;
-    
-    void setText(std::string newText);
-    
-    std:string getText() const { return text; }
-
-    protected:
-    Label(float x, float y, float w, float h, std::string txt);
-
-    private:
-    std::string text;
-    SDL_Texture* t
-};
+namespace demo {
+    class Label;
+    typedef std::shared_ptr<Label> LabelPtr;
+    class Label: public Sprite {
+        public:
+        static LabelPtr make(float x, float y, float w, float h, std::string txt);
+        ~Label() {};
+        void draw() const;
+        void setText(std::string newText);
+        std:string getText() const { return text; }
+        protected:
+        Label(float x, float y, float w, float h, std::string txt);
+        
+        private:
+        std::string text;
+        SDL_Texture* t
+    };
+}
 
 
 
