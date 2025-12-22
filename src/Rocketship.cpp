959b223aacfe8d0a76711a3201da35ae30292a90
Enes <enesihsancelik@gmail.com>
2025-12-22 12:36:06 +0100

    Adjusted rocketship image

--- src/Rocketship.cpp
+++ src/Rocketship.cpp
@@ -1,14 +1,19 @@
 #include "Rocketship.h"
 #include "Constants.h"
 
-Rocketship::Rocketship() : Sprite(constants::rocketship_str, 
-                                  (constants::gScreenWidth/2) - 25, 
-                                  constants::gScreenHeight - 100) {}
+Rocketship::Rocketship() : Sprite(constants::rocketship_str, 0, 0) {
+    rect.w = 64.f;
+    rect.h = 64.f;
+
+    rect.x = (constants::gScreenWidth / 2.0f) - (rect.w / 2.0f);
+    rect.y = constants::gScreenHeight - rect.h - 20.0f;
+
+}
 
 void Rocketship::tick() {
     const bool* keys = SDL_GetKeyboardState(NULL);
-    if (keys[SDL_SCANCODE_LEFT] && getRect().x > 0) move(-5, 0);
-    if (keys[SDL_SCANCODE_RIGHT] && getRect().x < (constants::gScreenWidth - getRect().w)) move(5, 0);
+    if (keys[SDL_SCANCODE_LEFT] && getRect().x > 0) move(-4, 0);
+    if (keys[SDL_SCANCODE_RIGHT] && getRect().x < (constants::gScreenWidth - getRect().w)) move(4, 0);
 }
 
 void Rocketship::onKeyDown(const SDL_Event& event) {
